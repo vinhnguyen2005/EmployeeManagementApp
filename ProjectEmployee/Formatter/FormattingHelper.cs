9a220af3c21b4b60073fbee15c6cd27d4a6fb920
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-06-20 14:10:54 +0700

    feat: add Formatter helper module

---
+++ ProjectEmployee/Formatter/FormattingHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEmployee.Formatter
+{
+    public static class FormattingHelper
+    {
+        public static string FormatAsInteger(decimal? value)
+        {
+            return value.HasValue ? Math.Floor(value.Value).ToString("N0") : string.Empty;
+        }
+
+        public static string FormatAsCurrency(decimal? value)
+        {
+            return value.HasValue ? value.Value.ToString("C") : string.Empty;
+        }
+
+        public static string FormatAsDollarInteger(decimal? value)
+        {
+            return value.HasValue ? $"${Math.Floor(value.Value).ToString("N0")}" : string.Empty;
+        }
+    }
+}
